0544f6d350535a0b5ef0b45143ab42437d60003c
Adam Ayash <mobiragegames@mail.com>
2024-05-27 21:34:08 -0700

    Още данни

--- TechStoreWebApplication/TechStoreWebApplication/Database/DbInitializer.cs
+++ TechStoreWebApplication/TechStoreWebApplication/Database/DbInitializer.cs
@@ -39,9 +39,7 @@
 
                     new Category() { Name = "Телефони" },
 
-                    new Category() { Name = "Процесори" },
-
-                    new Category() { Name = "ВидеоКарти" }
+                    new Category() { Name = "Процесори" }
                 );
 			}
 
@@ -51,7 +49,11 @@
 				(
 					new Product() { CategoryID = 1,Name= "Лаптоп Acer Nitro V16 ANV16-41-R3MD", ImageUrl = "https://ardes.bg/uploads/original/acer-nitro-v16-anv16-41-529298.jpg", Price=2199d },
                     new Product() { CategoryID = 1, Name = "Лаптоп Lenovo V15 G3 IAP", ImageUrl = "https://ardes.bg/uploads/original/lenovo-v15-g3-432234.jpg", Price = 949d },
-                    new Product() { CategoryID = 1, Name = "Лаптоп HP ZBook Fury 15 G8", ImageUrl = "https://ardes.bg/uploads/original/hp-zbook-fury-15-g7-300512.jpg", Price = 2499d }
+                    new Product() { CategoryID = 1, Name = "Лаптоп HP ZBook Fury 15 G8", ImageUrl = "https://ardes.bg/uploads/original/hp-zbook-fury-15-g7-300512.jpg", Price = 2499d },
+
+                    new Product() { CategoryID = 2, Name = "Смартфон Xiaomi Redmi Note 12 4G", ImageUrl = "https://ardes.bg/uploads/original/xiaomi-redmi-note-12-4g-446858.jpg", Price = 279d },
+
+                    new Product() { CategoryID = 2, Name = "Смартфон Apple iPhone 15 Pro Max", ImageUrl = "https://ardes.bg/uploads/original/apple-iphone-15-pro-490336.jpg", Price = 2689d }
                 );
 			}
 
